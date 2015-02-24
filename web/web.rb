9ade8d00c5c04418a49d06df6b8f3423a632813b
Cupertino Miranda <Cupertino.Miranda@synopsys.com>
2015-02-24 13:02:25 +0100

    Added some web interface experiments

---
+++ web/web.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+$:.push("#{Dir.pwd}/../")
+
+require 'dbSetup'
+ 
+get '/' do
+	@instruction = Instruction.all
+  haml :index
+end
+	   
