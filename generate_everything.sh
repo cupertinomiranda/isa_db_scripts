29723ef0b7188f9c4974ba0c1a19fa6248b86e32
Cupertino Miranda <Cupertino.Miranda@synopsys.com>
2015-08-26 18:19:35 +0200

    Latest state

--- generate_everything.sh
+++ generate_everything.sh
@@ -3,4 +3,4 @@
 erb templates/opc.c.erb > "$1/include/opcode/arc-func.h"
 erb templates/reloc.def.erb > "$1/include/elf/arc-reloc.def"
 ruby patch_reloc.c.rb $1/bfd/reloc.c
-indent -nbad -bap -nbc -bbo -bl -bli2 -bls -ncdb -nce -cp1 -cs -di2 -ndj -nfc1 -nfca -hnl -i2 -ip5 -lp -pcs -psl -nsc -nsob
+indent -nbad -bap -nbc -bbo -bl -bli2 -bls -ncdb -nce -cp1 -cs -di2 -ndj -nfc1 -nfca -hnl -i2 -ip5 -lp -pcs -psl -nsc -nsob "$1/include/opcode/arc-func.h" 
