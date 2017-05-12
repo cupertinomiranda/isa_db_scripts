16a4c146ae16ad787a520a614167f2f5acc45268
Cupertino Miranda <cmiranda@synopsys.com>
2017-05-12 14:55:36 +0200

    Fixed relocations

--- model/Relocation.rb
+++ model/Relocation.rb
@@ -54,11 +54,11 @@ Relocation.new("R_ARC_24            0x3   bits24    bitfield    S+A")
 Relocation.new("R_ARC_32            0x4   word32    bitfield    S+A")
 
 # Unsupported
-Relocation.new("R_ARC_N8            0x8   bits8     bitfield    S-A")
-Relocation.new("R_ARC_N16           0x9   bits16    bitfield    S-A")
-Relocation.new("R_ARC_N24           0xa   bits24    bitfield    S-A")
-Relocation.new("R_ARC_N32           0xb   word32    bitfield    S-A")
-Relocation.new("R_ARC_SDA           0xc   disp9     bitfield    S+A")
+Relocation.new("R_ARC_N8            0x8   bits8     bitfield    A-S")
+Relocation.new("R_ARC_N16           0x9   bits16    bitfield    A-S")
+Relocation.new("R_ARC_N24           0xa   bits24    bitfield    A-S")
+Relocation.new("R_ARC_N32           0xb   word32    bitfield    A-S")
+Relocation.new("R_ARC_SDA           0xc   disp9     bitfield    ME(S+A-_SDA_BASE_)")
 Relocation.new("R_ARC_SECTOFF       0xd   word32    bitfield    (S-SECTSTART)+A")
 
 # arcompact elf me reloc
@@ -85,32 +85,35 @@ Relocation.new("R_ARC_32_ME         0x1b  limm      signed      ME(S+A) (MES)")
 Relocation.new("R_ARC_32_ME_S       0x69  limms     signed      ME(S+A) (MES)")
 
 # Unsupported
-Relocation.new("R_ARC_N32_ME        0x1c  word32    bitfield    ME(S-A) (MES)")
+Relocation.new("R_ARC_N32_ME        0x1c  word32    bitfield    ME(A-S) (MES)")
 Relocation.new("R_ARC_SECTOFF_ME    0x1d  word32    bitfield    ME((S-SECTSTART)+A) (MES)")
 
 # arcompact elf me reloc
-Relocation.new("R_ARC_SDA32_ME      0x1e  limm      signed      ME((S+A)-_SDA_BASE_) (MES)")
+Relocation.new("R_ARC_SDA32_ME      0x1e  limm      signed      (S+A-_SDA_BASE_)")
 
 # Unsupported
 Relocation.new("R_ARC_W_ME          0x1f  word32    bitfield    ME((S+A)&~3) (word-aligned MES)")
-Relocation.new("R_AC_SECTOFF_U8     0x23  disp9ls   bitfield    S+A-SECTSTART")
-Relocation.new("R_AC_SECTOFF_U8_1   0x24  disp9ls   bitfield    (S+A-SECTSTART)>>1")
-Relocation.new("R_AC_SECTOFF_U8_2   0x25  disp9ls   bitfield    (S+A-SECTSTART)>>2")
-Relocation.new("R_AC_SECTFOFF_S9    0x26  disp9ls   bitfield    S+A-SECTSTART")
-Relocation.new("R_AC_SECTFOFF_S9_1  0x27  disp9ls   bitfield    (S+A-SECTSTART)>>1")
-Relocation.new("R_AC_SECTFOFF_S9_2  0x28  disp9ls   bitfield    (S+A-SECTSTART)>>2")
+Relocation.new("R_AC_SECTOFF_U8     0x23  disp9ls   bitfield    ME(S+A-SECTSTART)")
+Relocation.new("R_AC_SECTOFF_U8_1   0x24  disp9ls   bitfield    ME((S+A-SECTSTART)>>1)")
+Relocation.new("R_AC_SECTOFF_U8_2   0x25  disp9ls   bitfield    ME((S+A-SECTSTART)>>2)")
+
+Relocation.new("R_AC_SECTOFF_S9    0x26  disp9ls   bitfield    ME(S+A-SECTSTART-256)")
+Relocation.new("R_AC_SECTOFF_S9_1  0x27  disp9ls   bitfield    ME((S+A-SECTSTART-256)>>1)")
+Relocation.new("R_AC_SECTOFF_S9_2  0x28  disp9ls   bitfield    ME((S+A-SECTSTART-256)>>2)")
+
+
 Relocation.new("R_ARC_SECTOFF_ME_1  0x29  word32    bitfield    ME(((S-SECTSTART)+A)>>1) (MES)")
 Relocation.new("R_ARC_SECTOFF_ME_2  0x2a  word32    bitfield    ME(((S-SECTSTART)+A)>>2) (MES)")
 Relocation.new("R_ARC_SECTOFF_1     0x2b  word32    bitfield    ((S-SECTSTART)+A)>>1")
 Relocation.new("R_ARC_SECTOFF_2     0x2c  word32    bitfield    ((S-SECTSTART)+A)>>2")
 
-Relocation.new("R_ARC_SDA_12	    0x2d  disp12s   signed      (S+A-_SDA_BASE_)")
+Relocation.new("R_ARC_SDA_12	    0x2d  disp12s   signed      ME(S+A-_SDA_BASE_)")
 
 Relocation.new("R_ARC_SDA16_ST2     0x30  disp9s1   signed      (S+A-_SDA_BASE_)>>2 (Dsiambiguation for several relocations)")
 
 # arcompact elf me reloc
 Relocation.new("R_ARC_32_PCREL      0x31  word32    signed      S+A-PDATA")
-Relocation.new("R_ARC_PC32          0x32  word32    signed      S+A-P")
+Relocation.new("R_ARC_PC32          0x32  word32    signed      ME(S+A-P)")
 
 # Special
 Relocation.new("R_ARC_GOT32         0x3b  word32    dont        G+A") # == Special
@@ -135,8 +138,8 @@ Relocation.new("R_ARC_TLS_GD_GOT    0x45  word32    dont        ME(G+GOT-P)") # 
 Relocation.new("R_ARC_TLS_GD_LD     0x46  none      dont        0") # ,"R_ARC_TLS_GD_LD"),
 Relocation.new("R_ARC_TLS_GD_CALL   0x47  word32    dont        0") # ,"R_ARC_TLS_GD_CALL"),
 Relocation.new("R_ARC_TLS_IE_GOT    0x48  word32    dont        ME(G+GOT-P)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_IE_GOT",-1),
-Relocation.new("R_ARC_TLS_DTPOFF    0x43  word32    dont        ME(S-TLS_REL)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_DTPOFF",-1),
-Relocation.new("R_ARC_TLS_DTPOFF_S9 0x49  word32    dont        ME(S-TLS_REL)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_DTPOFF_S9",-1),
+Relocation.new("R_ARC_TLS_DTPOFF    0x43  word32    dont        ME(S-SECTSTART+A)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_DTPOFF",-1),
+Relocation.new("R_ARC_TLS_DTPOFF_S9 0x49  word32    dont        ME(S-SECTSTART+A)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_DTPOFF_S9",-1),
 Relocation.new("R_ARC_TLS_LE_S9     0x4a  word32    dont        ME(S+TCB_SIZE-TLS_REL)") # , 0, 2, 9, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_LE_S9",-1),
 Relocation.new("R_ARC_TLS_LE_32     0x4b  word32    dont        ME(S+A+TCB_SIZE-TLS_REL)") # , 0, 2, 32, FALSE, 0, arcompact_elf_me_reloc, "R_ARC_TLS_LE_32",-1),
 # WITHOUT TLS
